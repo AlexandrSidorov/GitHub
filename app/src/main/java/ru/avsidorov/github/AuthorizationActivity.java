f131a47dcb59c6fef398823ba5bfeacccae31646
Alexandr Sidorov <sidik211@gmail.com>
2015-04-03 14:24:54 +0300

    Add authorization with MaterialDialog on failed or success

--- app/src/main/java/ru/avsidorov/github/AuthorizationActivity.java
+++ app/src/main/java/ru/avsidorov/github/AuthorizationActivity.java
@@ -1,9 +1,23 @@
 package ru.avsidorov.github;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import retrofit.Callback;
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import ru.avsidorov.github.API.ApiGIT;
+import ru.avsidorov.github.MODELS.GHUser;
 
 
 public class AuthorizationActivity extends ActionBarActivity {
@@ -13,6 +27,51 @@ public class AuthorizationActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization);
         getSupportActionBar().hide();
+        final EditText evLogin = (EditText) findViewById(R.id.loginEditText);
+        final EditText evPassword = (EditText) findViewById(R.id.loginEditView);
+        Button btnLogin = (Button) findViewById(R.id.loginButton);
+        final MaterialDialog.Builder materialDialog = new MaterialDialog.Builder(this); //костыль какой-то, но контексты через getBaseContext() или getApplicationContext в OnClickListener не принимает
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String login = evLogin.getText().toString();
+                final String password = evPassword.getText().toString();
+                if (!login.isEmpty() && !password.isEmpty()) {
+
+                    RestAdapter restAdapter = new RestAdapter.Builder()
+                            .setRequestInterceptor(new RequestInterceptor() {
+                                @Override
+                                public void intercept(RequestFacade request) {
+                                    final String authorizationValue = encodeCredentialsForBasicAuthorization(login, password);
+                                    request.addHeader("Authorization", authorizationValue);
+
+                                }
+                            })
+                            .setEndpoint(Constants.ENDPOINT)
+                            .setLogLevel(RestAdapter.LogLevel.HEADERS).build();
+                    ApiGIT apiGIT = restAdapter.create(ApiGIT.class);
+                    apiGIT.getUser(login, new Callback<GHUser>() {
+                        @Override
+                        public void success(GHUser ghUser, Response response) {
+                            materialDialog.title("Ура").content(ghUser.getName()).show();
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            materialDialog.title("Ошибка").content(error.getLocalizedMessage()).show();
+                        }
+                    });
+                } else {
+                    materialDialog
+                            .title(R.string.dialog_title_no_login_or_password)
+                            .content(R.string.dialog_content_enter_login_and_password)
+                            .positiveText(R.string.ok)
+                            .show();
+
+
+                }
+            }
+        });
     }
 
 
@@ -37,4 +96,10 @@ public class AuthorizationActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private String encodeCredentialsForBasicAuthorization(String login, String password) {
+        final String userAndPassword = login + ":" + password;
+        return "Basic " + Base64.encodeToString(userAndPassword.getBytes(), Base64.NO_WRAP);
+    }
+
 }
---
+++ app/src/main/java/ru/avsidorov/github/Constants.java
@@ -0,0 +1,10 @@
+package ru.avsidorov.github;
+
+/**
+ * Created by Сидоров on 03.04.2015.
+ */
+public class Constants {
+    public final static String ENDPOINT = "https://api.github.com";
+
+
+}
